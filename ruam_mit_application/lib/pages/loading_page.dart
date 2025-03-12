46803b27207dd30a16f38d66a9466878589c1054
Kanyanat Uetrakulchai <kanyanat.uet@student.mahidol.ac.th>
2025-03-12 18:35:46 +0700

    Loading Page Complete

--- ruam_mit_application/lib/pages/loading_page.dart
+++ ruam_mit_application/lib/pages/loading_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+// import 'package:flutter_svg/flutter_svg.dart';
 import 'package:typewritertext/typewritertext.dart';
 
 
@@ -11,29 +11,38 @@ class LoadingPage extends StatelessWidget {
     return 
     Scaffold(
       backgroundColor: const Color(0xffD63939),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Image.asset(
-            'assets/app_logo.jpg'
-          ),
-          // SvgPicture.asset(
-          //   'assets/app_logo.svg',
-          //   // width: 65,
-          //   // height: 65,
-          //   allowDrawingOutsideViewBox: true,
-          // ),
-          // SizedBox(height: 20,),
-          TypeWriter.text(
-            'loading . . .',
-            style: TextStyle(
-              color: Colors.white,
+      body: Center( 
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          // crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image(
+              image: AssetImage('assets/app_logo.png')
             ),
-            duration: const Duration(milliseconds: 50),
-            repeat: true,
-          )
-      ],),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'loading ',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                TypeWriter.text(
+                  '. . . . ',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                  duration: const Duration(milliseconds: 50),
+                  repeat: true,
+                )
+              ],
+              
+            )
+            
+          ],
+        )
+      ),
     );
   }
 }
